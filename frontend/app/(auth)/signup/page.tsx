c22cfcd62acf9335550077d2e69bb44e9c226cf4
Daryll Castelino <daryllcastelino007@gmail.com>
2025-08-22 19:36:26 +1000

    complete participant tab ui

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,14 +1,5 @@
-import { AppWindowIcon, CodeIcon } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -17,8 +8,8 @@ const Signup = () => {
     return (
         <div className="flex flex-col gap-6">
             <Card className="bg-primary px-3 py-6 w-lg h-[32rem] p-12">
-                <form action="#" method="post">
-                    <Tabs defaultValue="host" className="w-full gap-10">
+                <form action="#" method="post" className="h-full">
+                    <Tabs defaultValue="host" className=" h-full gap-10">
                         <TabsList className="grid w-full grid-cols-2 bg-secondary">
                             <TabsTrigger
                                 value="host"
@@ -33,7 +24,10 @@ const Signup = () => {
                                 PARTICIPANT
                             </TabsTrigger>
                         </TabsList>
-                        <TabsContent value="host">
+                        <TabsContent
+                            value="host"
+                            className="flex flex-col justify-between"
+                        >
                             <CardContent className="grid gap-6">
                                 <div className="grid gap-3">
                                     <Label htmlFor="email">Email</Label>
@@ -60,39 +54,35 @@ const Signup = () => {
                                 </Button>
                             </CardFooter>
                         </TabsContent>
-                        <TabsContent value="participant">
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Password</CardTitle>
-                                    <CardDescription>
-                                        Change your password here. After saving,
-                                        you&apos;ll be logged out.
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent className="grid gap-6">
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="tabs-demo-current">
-                                            Current password
-                                        </Label>
-                                        <Input
-                                            id="tabs-demo-current"
-                                            type="password"
-                                        />
-                                    </div>
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="tabs-demo-new">
-                                            New password
-                                        </Label>
-                                        <Input
-                                            id="tabs-demo-new"
-                                            type="password"
-                                        />
-                                    </div>
-                                </CardContent>
-                                <CardFooter>
-                                    <Button>Save password</Button>
-                                </CardFooter>
-                            </Card>
+                        <TabsContent
+                            value="participant"
+                            className="flex flex-col justify-between"
+                        >
+                            <CardContent className="grid gap-6">
+                                <div className="grid gap-3">
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input
+                                        id="username"
+                                        type="username"
+                                        className="bg-secondary"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-3">
+                                    <Label htmlFor="gameCode">Game Code</Label>
+                                    <Input
+                                        id="gameCode"
+                                        type="text"
+                                        className="bg-secondary"
+                                        required
+                                    />
+                                </div>
+                            </CardContent>
+                            <CardFooter className="flex justify-center mt-10">
+                                <Button className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer">
+                                    Join
+                                </Button>
+                            </CardFooter>
                         </TabsContent>
                     </Tabs>
                 </form>
