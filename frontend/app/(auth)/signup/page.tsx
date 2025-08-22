453808d8ee85cf8f8d55114051ec739370099a84
Daryll Castelino <daryllcastelino007@gmail.com>
2025-08-22 21:53:27 +1000

    minor fix: complete participant tab UI

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -5,28 +5,48 @@ import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Signup = () => {
+    const handleLogin = async (formData: FormData) => {
+        "use server";
+
+        const rawData = {
+            email: formData.get("email"),
+            password: formData.get("password"),
+        };
+        console.log("Login Data:", rawData);
+    };
+
+    const handleJoin = async (formData: FormData) => {
+        "use server";
+
+        const rawData = {
+            username: formData.get("username"),
+            gameCode: formData.get("gameCode"),
+        };
+        console.log("Join Data:", rawData);
+    };
+
     return (
         <div className="flex flex-col gap-6">
             <Card className="bg-primary px-3 py-6 w-lg h-[32rem] p-12">
-                <form action="#" method="post" className="h-full">
-                    <Tabs defaultValue="host" className=" h-full gap-10">
-                        <TabsList className="grid w-full grid-cols-2 bg-secondary">
-                            <TabsTrigger
-                                value="host"
-                                className="data-[state=active]:bg-primary data-[state=active]:font-bold"
-                            >
-                                HOST
-                            </TabsTrigger>
-                            <TabsTrigger
-                                value="participant"
-                                className="data-[state=active]:bg-primary data-[state=active]:font-bold"
-                            >
-                                PARTICIPANT
-                            </TabsTrigger>
-                        </TabsList>
+                <Tabs defaultValue="host" className="h-full gap-10">
+                    <TabsList className="grid w-full grid-cols-2 bg-secondary">
+                        <TabsTrigger
+                            value="host"
+                            className="data-[state=active]:bg-primary data-[state=active]:font-bold"
+                        >
+                            HOST
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="participant"
+                            className="data-[state=active]:bg-primary data-[state=active]:font-bold"
+                        >
+                            PARTICIPANT
+                        </TabsTrigger>
+                    </TabsList>
+                    <form action={handleLogin} method="post" className="h-full">
                         <TabsContent
                             value="host"
-                            className="flex flex-col justify-between"
+                            className="flex flex-col justify-between h-full"
                         >
                             <CardContent className="grid gap-6">
                                 <div className="grid gap-3">
@@ -49,11 +69,16 @@ const Signup = () => {
                                 </div>
                             </CardContent>
                             <CardFooter className="flex justify-center mt-10">
-                                <Button className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer">
+                                <Button
+                                    type="submit"
+                                    className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer"
+                                >
                                     Login
                                 </Button>
                             </CardFooter>
                         </TabsContent>
+                    </form>
+                    <form action={handleJoin} method="post" className="h-full">
                         <TabsContent
                             value="participant"
                             className="flex flex-col justify-between"
@@ -84,8 +109,8 @@ const Signup = () => {
                                 </Button>
                             </CardFooter>
                         </TabsContent>
-                    </Tabs>
-                </form>
+                    </form>
+                </Tabs>
             </Card>
         </div>
     );
