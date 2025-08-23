4ea41695af0b3c3f85273ece1f5a2643515b139c
Daryll Castelino <daryllcastelino007@gmail.com>
2025-08-23 19:55:31 +1000

    minor tweaks

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -28,7 +28,10 @@ const Signup = () => {
     return (
         <div className="flex flex-col gap-6">
             <Card className="bg-primary px-3 py-6 w-lg h-[32rem] p-12">
-                <Tabs defaultValue="host" className="h-full gap-10">
+                <Tabs
+                    defaultValue="host"
+                    className="flex flex-col justify-between gap-10 h-full"
+                >
                     <TabsList className="grid w-full grid-cols-2 bg-secondary">
                         <TabsTrigger
                             value="host"
@@ -43,73 +46,70 @@ const Signup = () => {
                             PARTICIPANT
                         </TabsTrigger>
                     </TabsList>
-                    <form action={handleLogin} method="post" className="h-full">
-                        <TabsContent
-                            value="host"
-                            className="flex flex-col justify-between h-full"
-                        >
-                            <CardContent className="grid gap-6">
-                                <div className="grid gap-3">
-                                    <Label htmlFor="email">Email</Label>
-                                    <Input
-                                        id="email"
-                                        type="email"
-                                        className="bg-secondary"
-                                        required
-                                    />
-                                </div>
-                                <div className="grid gap-3">
-                                    <Label htmlFor="password">Password</Label>
-                                    <Input
-                                        id="password"
-                                        type="password"
-                                        className="bg-secondary"
-                                        required
-                                    />
-                                </div>
-                            </CardContent>
-                            <CardFooter className="flex justify-center mt-10">
-                                <Button
-                                    type="submit"
-                                    className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer"
-                                >
-                                    Login
-                                </Button>
-                            </CardFooter>
-                        </TabsContent>
-                    </form>
-                    <form action={handleJoin} method="post" className="h-full">
-                        <TabsContent
-                            value="participant"
-                            className="flex flex-col justify-between"
-                        >
-                            <CardContent className="grid gap-6">
-                                <div className="grid gap-3">
-                                    <Label htmlFor="username">Username</Label>
-                                    <Input
-                                        id="username"
-                                        type="username"
-                                        className="bg-secondary"
-                                        required
-                                    />
-                                </div>
-                                <div className="grid gap-3">
-                                    <Label htmlFor="gameCode">Game Code</Label>
-                                    <Input
-                                        id="gameCode"
-                                        type="text"
-                                        className="bg-secondary"
-                                        required
-                                    />
-                                </div>
-                            </CardContent>
-                            <CardFooter className="flex justify-center mt-10">
-                                <Button className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer">
-                                    Join
-                                </Button>
-                            </CardFooter>
-                        </TabsContent>
-                    </form>
+                    <TabsContent
+                        value="host"
+                        className="flex flex-col justify-between h-full"
+                    >
+                        <CardContent className="grid gap-6">
+                            <div className="grid gap-3">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    className="bg-secondary"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="password">Password</Label>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    className="bg-secondary"
+                                    required
+                                />
+                            </div>
+                        </CardContent>
+                        <CardFooter className="flex justify-center mt-10">
+                            <Button
+                                onClick={handleLogin()}
+                                type="submit"
+                                className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer"
+                            >
+                                Login
+                            </Button>
+                        </CardFooter>
+                    </TabsContent>
+                    <TabsContent
+                        value="participant"
+                        className="flex flex-col justify-between h-full"
+                    >
+                        <CardContent className="grid gap-6">
+                            <div className="grid gap-3">
+                                <Label htmlFor="username">Username</Label>
+                                <Input
+                                    id="username"
+                                    type="username"
+                                    className="bg-secondary"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-3">
+                                <Label htmlFor="gameCode">Game Code</Label>
+                                <Input
+                                    id="gameCode"
+                                    type="text"
+                                    className="bg-secondary"
+                                    required
+                                />
+                            </div>
+                        </CardContent>
+                        <CardFooter className="flex justify-center mt-10">
+                            <Button className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer">
+                                Join
+                            </Button>
+                        </CardFooter>
+                    </TabsContent>
                 </Tabs>
             </Card>
         </div>
