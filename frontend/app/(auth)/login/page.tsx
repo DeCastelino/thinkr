87c60b4fccd894a838747375372902a28ce19619
Daryll Castelino <daryllcastelino007@gmail.com>
2025-08-24 11:31:22 +1000

    complete login UI

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,30 +1,101 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const Login = () => {
+    // ADD LOGIN LOGIC HERE
+
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1>Login</h1>
-            <form
-                action="/login"
-                method="POST"
-                className="flex items-center flex-col space-y-4"
-            >
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" name="username" required />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        required
-                    />
-                </div>
-                <div>
-                    <button type="submit">Log In</button>
-                </div>
-            </form>
-        </div>
+        <form action="">
+            <div className="flex flex-col gap-6">
+                <Card className="bg-primary px-3 py-6 w-lg h-[32rem] p-12">
+                    <Tabs
+                        defaultValue="host"
+                        className="flex flex-col justify-between gap-10 h-full"
+                    >
+                        <TabsList className="grid w-full grid-cols-2 bg-secondary">
+                            <TabsTrigger
+                                value="host"
+                                className="data-[state=active]:bg-primary data-[state=active]:font-bold"
+                            >
+                                HOST
+                            </TabsTrigger>
+                            <TabsTrigger
+                                value="participant"
+                                className="data-[state=active]:bg-primary data-[state=active]:font-bold"
+                            >
+                                PARTICIPANT
+                            </TabsTrigger>
+                        </TabsList>
+                        <TabsContent
+                            value="host"
+                            className="flex flex-col justify-between h-full"
+                        >
+                            <CardContent className="grid gap-6">
+                                <div className="grid gap-3">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        className="bg-secondary"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-3">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        className="bg-secondary"
+                                        required
+                                    />
+                                </div>
+                            </CardContent>
+                            <CardFooter className="flex justify-center mt-10">
+                                <Button
+                                    type="submit"
+                                    className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer"
+                                >
+                                    Login
+                                </Button>
+                            </CardFooter>
+                        </TabsContent>
+                        <TabsContent
+                            value="participant"
+                            className="flex flex-col justify-between h-full"
+                        >
+                            <CardContent className="grid gap-6">
+                                <div className="grid gap-3">
+                                    <Label htmlFor="username">Username</Label>
+                                    <Input
+                                        id="username"
+                                        type="username"
+                                        className="bg-secondary"
+                                        required
+                                    />
+                                </div>
+                                <div className="grid gap-3">
+                                    <Label htmlFor="gameCode">Game Code</Label>
+                                    <Input
+                                        id="gameCode"
+                                        type="text"
+                                        className="bg-secondary"
+                                        required
+                                    />
+                                </div>
+                            </CardContent>
+                            <CardFooter className="flex justify-center mt-10">
+                                <Button className="w-full bg-foreground outline-none shadow-none hover:bg-inherit hover:text-foreground hover:border-2 hover:border-foreground hover:cursor-pointer">
+                                    Join
+                                </Button>
+                            </CardFooter>
+                        </TabsContent>
+                    </Tabs>
+                </Card>
+            </div>
+        </form>
     );
 };
 
