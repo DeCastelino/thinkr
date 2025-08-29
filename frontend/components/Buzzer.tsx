764baacf7634f960c216d4a25abea76a2834cfed
Daryll Castelino <daryllcastelino007@gmail.com>
2025-08-29 20:53:42 +1000

    buzzer UI update

--- frontend/components/Buzzer.tsx
+++ frontend/components/Buzzer.tsx
@@ -1,6 +1,6 @@
 const Buzzer = () => {
     return (
-        <div className=" flex text-7xl bg-primary rounded-full justify-center items-center p-4 w-[26rem] h-[26rem]">
+        <div className=" flex text-7xl bg-primary rounded-full justify-center items-center p-4 w-[26rem] h-[26rem] hover:bg-black hover:text-white hover:cursor-pointer select-none">
             Buzzer
         </div>
     );
